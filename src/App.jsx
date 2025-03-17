c032c22f29955dfcd6544a15dd4cc839e6e2e3fa
Emily Pearl <Emilypearl9193@gmail.com>
2025-03-16 19:47:07 -0600

    using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 
 import './App.css'
-
+// creating a reuseable component to be able to pass data when user clicks the square
+//pass the value prop 
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+ }
+ 
 
 function App() {
 
@@ -8,21 +13,25 @@ function App() {
     {/* tic tac toe board ,
     started with nine buttons that all had an x and then split them into 
     rows and numbered them to create the board */}
-    <div className="board-row">
-      <button className="square">1</button>
-      <button className="square">2</button>
-      <button className="square">3</button>
+
+   {/* passing the value prop to each box */}
+  
+     <div className="board-row">
+     <Square value="1" />
+     <Square value="2" />
+     <Square value="3" />
    </div>
    <div className="board-row">
-      <button className="square">4</button>
-      <button className="square">5</button>
-      <button className="square">6</button>
+     <Square value="4" />
+     <Square value="5" />
+     <Square value="6" />
    </div>
    <div className="board-row">
-      <button className="square">7</button>
-      <button className="square">8</button>
-      <button className="square">9</button>
+     <Square value="7" />
+     <Square value="8" />
+     <Square value="9" />
    </div>
+
  </>
   
 }
