a0dcde5e162612030fc374e1ed18b4c91fde050c
Emily Pearl <Emilypearl9193@gmail.com>
2025-03-16 19:37:45 -0600

    creating the board using nine boxes, splitting them up into rows and numbering each box

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,26 @@ import './App.css'
 
 function App() {
 
-    return <button className="square">X</button>;
-
- 
+    return <>
+    {/* tic tac toe board ,
+    started with nine buttons that all had an x and then split them into 
+    rows and numbered them to create the board */}
+    <div className="board-row">
+      <button className="square">1</button>
+      <button className="square">2</button>
+      <button className="square">3</button>
+   </div>
+   <div className="board-row">
+      <button className="square">4</button>
+      <button className="square">5</button>
+      <button className="square">6</button>
+   </div>
+   <div className="board-row">
+      <button className="square">7</button>
+      <button className="square">8</button>
+      <button className="square">9</button>
+   </div>
+ </>
   
 }
 
