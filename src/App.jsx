5f68aca16c15ac1318432a19b175ec80e362be81
Emily Pearl <Emilypearl9193@gmail.com>
2025-03-22 18:07:20 -0600

    game state moved to parent to allow child component data to be collected

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import { useState } from 'react';
 import './App.css'
 //creating a reuseable component to be able to pass data when user clicks the square 
-function Square() {
-//removed the value prop and replaced it with useState to change the box to have an "x" when clicked
-const [value,setValue] = useState(null);
-  //interactive component
-function handleClick(){
-  // square should display "x" when clicked
-  setValue('X');
+function Square({value}) {
+  return <button className="square">{value}</button>;
 }
-  return <button className="square" onClick={handleClick}>{value}</button>;
- }
  
 
 function App() {
-
+  // declaring a shared state in the parent component to collect data from child components
+  const [squares, setSquares] = useState(Array(9).fill(null));
     return <>
     {/* tic tac toe board ,
     started with nine buttons that all had an x and then split them into 
@@ -23,19 +17,19 @@ function App() {
    {/* passing the value prop to each box */}
   
      <div className="board-row">
-     <Square />
-     <Square />
-     <Square />
+     <Square value={squares[0]}/>
+     <Square value={squares[1]} />
+     <Square value={squares[2]}/>
    </div>
    <div className="board-row">
-     <Square />
-     <Square />
-     <Square />
+     <Square value={squares[3]}/>
+     <Square value={squares[4]}/>
+     <Square value={squares[5]}/>
    </div>
    <div className="board-row">
-     <Square />
-     <Square />
-     <Square />
+     <Square value={squares[6]}/>
+     <Square value={squares[7]}/>
+     <Square value={squares[8]}/>
    </div>
 
  </>
