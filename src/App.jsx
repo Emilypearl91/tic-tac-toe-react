a42985825109452779868a9052442f43de44f05e
Emily Pearl <Emilypearl9193@gmail.com>
2025-03-26 00:25:05 -0600

    winner declaration and player turns displayed

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,10 @@ function App() {
   // defining the handle click to update the array 
   // i argument takes the index of square to update
   function handleClick(i) {
-  // to ensure that a turn doesnt get overwritten, check if the square already has a value
-    if (squares[i]) {
-      return;
-    }
+  // to ensure that a turn doesn't get overwritten, check if the square already has a value snd if a player has won
+  if (squares[i] || calculateWinner(squares)) {
+    return;}
+
     const nextSquares = squares.slice();
     nextSquares[i] = "X";
     // each time a square is clicked , xIsNext boolean will flip causing the value to change from "x" to "o"
@@ -33,7 +33,37 @@ function App() {
 
     setXIsNext(!xIsNext);
   }
+  // this function checks if there is a winner by taking all the values of the squares and calculates a winner
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  // calculate who is the winner and which players tun it is
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
     return <>
+    {/* game status to display players turn and who the winner is  */}
+    <div className="status">{status}</div>
     {/* tic tac toe board */}
 
    {/* passing the value prop to each box */}
